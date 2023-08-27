28a7a0d27fdedd05fbf7692a7f9abda3af54d4cd
jumosd <jumosd@icloud.com>
2023-08-27 22:02:40 +0900

    투두리스트 추가 삭제 기능

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,17 +1,61 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
+
+
 const Main = () => {
-  const todo = useRef('');
+
+  const [todoItems, setTodoItems] = useState([])
+  const [selectFilter, setSelectFilter] = useState()
+  const [allFilterclicked, setAllFilterclicked] = useState(true);
+  const [todoFilterclicked, setTodoFilterclicked] = useState(false);
+  const [doneFilterclicked, setDoneFilterclicked] = useState(false);
+
   const createTodolist = (event) => {
     if (event.key === 'Enter') {
-      console.log(event.target.value);
+      setTodoItems([...todoItems,
+      event.target.value]);
+      event.target.value = ''
     }
   };
+
+  const deleteItem = (index) => {
+    const newTodoItems = [...todoItems];
+    newTodoItems.splice(index, 1);
+    setTodoItems(newTodoItems);
+  }
+  const allFilterHandler = () => {
+    setAllFilterclicked((prev) => !prev)
+  }
+  const todoFilterHandler = () => {
+    setTodoFilterclicked((prev) => !prev)
+
+  }
+  const doneFilterHandler = () => {
+    setDoneFilterclicked((prev) => !prev)
+
+  }
+
   return (
     <>
       <Container>
-        <TodoInput ref={todo} onKeyDown={createTodolist}></TodoInput>
+        <TodoInput onKeyPress={createTodolist}></TodoInput>
+        <Filter>
+          <div></div>
+          <FilterMenu>
+            <div onClick={allFilterHandler} allFilterclicked={allFilterclicked}>all</div>
+            <div onClick={todoFilterHandler}>todo</div>
+            <div onClick={doneFilterHandler}>done</div>
+          </FilterMenu>
+        </Filter>
+        {todoItems.map((item, idx) =>
+          <Todoitem key={idx}>
+            <div>{item}</div>
+            <button onClick={() => deleteItem(idx)}>삭제</button>
+          </Todoitem>
+        )
+        }
+
       </Container>
     </>
   );
@@ -20,15 +64,51 @@ const Main = () => {
 export default Main;
 
 const Container = styled.div`
-  display: flex;
-  height: 90px;
-  padding: 10px 32px;
-  align-items: center;
-  gap: 10px;
-  align-self: stretch;
-`;
+width: 750px;
+ margin: auto;
+`
 
 const TodoInput = styled.input`
+  font-size:2rem;
+  color: white;
   width: 750px;
   height: 65px;
+  margin-bottom: 10px;
+  border-radius: 8px;
+  border: 1px solid var(--dark-g-5, #4E5259);
+  background: var(--dark-g-3, #1E1D23);
 `;
+
+const Todoitem = styled.div`
+color:white;
+border-radius: 8px;
+background: var(--dark-g-35, #212529);
+margin: auto;
+margin-bottom: 10px;
+width: 750px;
+display: flex;
+justify-content: space-between;
+padding: 10px;
+
+
+`
+
+const Filter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+`
+
+const FilterMenu = styled.div`
+  display: flex;
+  div{
+    cursor: pointer;
+    margin-left: 1rem;
+    color: ${props => (props.allFilterclicked ? "red" : "white")};
+    font-family: Roboto;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    }
+`
