1535ba3a9bc19f1e3754f00e8f84dd70809fba2b
하진수 <81242485+jumosd@users.noreply.github.com>
2023-08-27 18:10:23 +0900

    d

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from '@emotion/styled';
 
-import Input from '../Input';
-
 const Main = () => {
-    return (
-        <>
-            <Container>
-
-                <Input></Input>
-            </Container>
-        </>
-    );
+  const todo = useRef('');
+  const createTodolist = (event) => {
+    if (event.key === 'Enter') {
+      console.log(event.target.value);
+    }
+  };
+  return (
+    <>
+      <Container>
+        <TodoInput ref={todo} onKeyDown={createTodolist}></TodoInput>
+      </Container>
+    </>
+  );
 };
 
 export default Main;
 
 const Container = styled.div`
-    display: flex;
-height: 90px;
-padding: 10px 32px;
-align-items: center;
-gap: 10px;
-align-self: stretch;
-`
+  display: flex;
+  height: 90px;
+  padding: 10px 32px;
+  align-items: center;
+  gap: 10px;
+  align-self: stretch;
+`;
+
+const TodoInput = styled.input`
+  width: 750px;
+  height: 65px;
+`;
