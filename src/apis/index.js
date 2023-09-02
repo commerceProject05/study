13e5d3afb59692c143d2b4c58d28dc786a46a618
kimseunggyu <dev.seunggyu@gmail.com>
2023-09-02 23:41:08 +0900

    설명 기반 예제 추가

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,8 +1,103 @@
 import axios from 'axios';
 
+// 설정 A
 export const client = axios.create({
   baseURL: 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+// jwt!
+// client.interceptors.request = (config) => {
+//   console.log('모든 요청전에 여기에서 정의한 내용을 수행해!');
+//
+//   // 로그인 유무로 jwt 토큰 값을 가져옴
+//   // jwt 토큰 값이 있으면
+//   if (jwt) {
+//     config.headers.Authorization = `Bearer ${jwt}`;
+//   }
+// };
+
+// 설정 B
+// export const client2 = axios.create({
+//   baseURL: 'http://localhost:8080',
+//   headers: {
+//     'Content-Type': 'multipart/form-data',
+//   },
+// });
+//
+// // /todo => 'application/json'
+// client.post('/todo', {
+//   name: 'value',
+// });
+//
+// axios.post(
+//   'http://localhost:8080/todo',
+//   {
+//     name: 'value',
+//   },
+//   {
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   },
+// );
+//
+// axios.delete('http://localhost:8080/todo', {
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+//
+// axios.patch(
+//   'http://localhost:8080/todo',
+//   {},
+//   {
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   },
+// );
+//
+// // /photo => 'multipart/form-data'
+// const formData = new FormData();
+// formData.append('image', file);
+// client2.post('/photo', formData);
+//
+// axios.post(
+//   'http://localhost:8080/photo',
+//   {
+//     name: 'value',
+//   },
+//   {
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   },
+// );
+//
+// axios.delete('http://localhost:8080/todo', {
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+//
+// axios.patch(
+//   'http://localhost:8080/todo',
+//   {},
+//   {
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   },
+// );
+
+// http://localhost:8080/todos
+// 전체에서 사용할 수 있는 axios
+// const todos = axios.get('http://localhost:8080/todos');
+// axios.post('http://localhost:8080/todo', {data})
+
+// 설정 A 가 적용된 axios
+// client.get('/todos');
+// client.post('/todos');
--- src/apis/todos.js
+++ src/apis/todos.js
@@ -5,16 +5,31 @@ export async function getTodos() {
   return response.data;
 }
 
+// payload === {
+//     "id": 10,
+//     "content": "test",
+//     "done": false
+// }
 export async function createTodo(payload) {
   const response = await client.post('/todos', payload);
   return response.data;
 }
 
+// todoId === 1,
+// payload === {
+//     "completed": false
+// }
 export async function updateTodo(todoId, payload) {
   const response = await client.patch(`/todos/${todoId}`, payload);
   return response.data;
+  //   response.data === {
+  //     "id": 1,
+  //     "content": "HTML",
+  //     "completed": false
+  // }
 }
 
+// todoId === 1
 export function deleteTodo(todoId) {
   return client.delete(`/todos/${todoId}`);
 }
